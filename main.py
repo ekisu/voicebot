72469056948c27b95a91c9c0eb1fac485fc07efb
ElegantMonkey <ramonzitosdesorte@hotmail.com>
2018-07-25 16:13:09 -0300

    asdsad

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class VoiceEntry:
         self.channel = msgCtx.channel
         self.player = player
         self.tempFile = tempFile
-        self.copycolaTexto = None
+        self.copycolaTexto = copycolaTexto
 
 class VoiceState:
     def __init__(self, bot):
@@ -253,7 +253,7 @@ class Voice:
         copycolaName = ctx.message.content[3:]
         if copycolaName == "":
             return
-            
+
         state = self.get_voice_state(ctx.message.server)
         if state.voice is None:
             success = await ctx.invoke(self.summon)
@@ -273,7 +273,7 @@ class Voice:
 
     @c.command(pass_context=True, no_pm=True, name="add")
     async def addCopycola(self, ctx, name, *, texto: str):
-        tts = gTTS(ctx.message.content, lang=TTS_LANGUAGE)
+        tts = gTTS(texto, lang=TTS_LANGUAGE)
         try:
             with open("copycola/{}.txt".format(name), "w") as ftext, open("copycola/{}.mp3".format(name), "wb") as fmp3:
                 await self.bot.loop.run_in_executor(EXECUTOR, tts.write_to_fp, fmp3)
